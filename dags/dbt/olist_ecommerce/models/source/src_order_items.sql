43ff35a31869f65b5232f36bdde5c38bcc94a784
kennethchinedu <anamsken60@gmail.com>
2024-04-07 10:31:44 +0100

    set up data sources

--- dags/dbt/olist_ecommerce/models/source/src_order_items.sql
+++ dags/dbt/olist_ecommerce/models/source/src_order_items.sql
@@ -1,8 +1,18 @@
+WITH order_items AS (
+    select
+        *
+    from
+        {{ source('olist', 'order_items') }}
+)
 
-select
-    *
-from
-{{ source('olist', 'order_items') }}
 
-
-
+SELECT 
+    order_id,
+    order_item_id,
+    product_id,
+    seller_id,
+    TO_TIMESTAMP(shipping_limit_date, 'DD/MM/YYYY HH24:MI') AS shipping_limit_date,
+    price,
+    freight_value
+FROM 
+    order_items
